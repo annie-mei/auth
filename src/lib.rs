8f77b6656928f833f5cf76fe8b0eb40c876abfd7
InfernapeXavier <itsgood37@ymail.com>
2023-02-26 08:14:05 +0000

    add redirect for authorization code

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,38 @@
 extern crate rocket;
 
 use anyhow::anyhow;
-use rocket::response::status::BadRequest;
-use rocket::State;
+use rocket::{
+    response::{status::BadRequest, Redirect},
+    State,
+};
 use shuttle_secrets::SecretStore;
+use url::Url;
 
 #[get("/secret")]
 async fn secret(state: &State<MyState>) -> Result<String, BadRequest<String>> {
-    Ok(state.secret.clone())
+    Ok(state.client_id.clone())
+}
+
+#[get("/login")]
+async fn login(state: &State<MyState>) -> Redirect {
+    // https://anilist.co/api/v2/oauth/authorize?
+    // client_id' => '{client_id}',
+    // 'redirect_uri' => '{redirect_uri}', // http://example.com/callback
+    // 'response_type' => 'code'
+
+    const ANILIST_BASE: &str = "https://anilist.co/api/v2/oauth/authorize";
+    let params = [
+        ("client_id", state.client_id.as_str()),
+        ("redirect_uri", "http://127.0.0.1:8000/authorized"),
+        ("response_type", "code"),
+    ];
+    let url = Url::parse_with_params(ANILIST_BASE, &params).unwrap();
+
+    Redirect::to(url.to_string())
 }
 
 struct MyState {
-    secret: String,
+    client_id: String,
 }
 
 #[shuttle_service::main]
@@ -20,14 +41,16 @@ async fn rocket(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_service::ShuttleRocket {
     // get secret defined in `Secrets.toml` file.
-    let secret = if let Some(secret) = secret_store.get("ANILIST_CLIENT_ID") {
-        secret
+    let client_id = if let Some(client_id) = secret_store.get("ANILIST_CLIENT_ID") {
+        client_id
     } else {
         return Err(anyhow!("secret was not found").into());
     };
 
-    let state = MyState { secret };
-    let rocket = rocket::build().mount("/", routes![secret]).manage(state);
+    let state = MyState { client_id };
+    let rocket = rocket::build()
+        .mount("/", routes![secret, login])
+        .manage(state);
 
     Ok(rocket)
 }
