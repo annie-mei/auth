e049ab1c2c8b23bc5d8623e49f41404b7ee5053d
InfernapeXavier <itsgood37@ymail.com>
2023-02-27 03:13:25 +0000

    fetch access token

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #[macro_use]
 extern crate rocket;
 
+use std::collections::HashMap;
+
 use anyhow::anyhow;
 use rocket::{
     response::{status::BadRequest, Redirect},
+    serde::Deserialize,
     State,
 };
 use shuttle_secrets::SecretStore;
@@ -32,8 +35,59 @@ async fn login(state: &State<MyState>) -> Redirect {
     Redirect::to(url.to_string())
 }
 
+#[get("/authorized?<code>")]
+async fn authorized(code: String, state: &State<MyState>) -> Result<String, BadRequest<String>> {
+    //     $response = $http->post('https://anilist.co/api/v2/oauth/token', [
+    //     'form_params' => [
+    //         'grant_type' => 'authorization_code',
+    //         'client_id' => '{client_id}',
+    //         'client_secret' => '{client_secret}',
+    //         'redirect_uri' => '{redirect_uri}', // http://example.com/callback
+    //         'code' => '{code}', // The Authorization code received previously
+    //     ],
+    //     'headers' => [
+    //         'Accept' => 'application/json'
+    //     ]
+    // ]);
+
+    #[derive(Debug, Deserialize)]
+    #[serde(crate = "rocket::serde")]
+    struct TokenResponse {
+        token_type: String,
+        expires_in: i32,
+        access_token: String,
+        refresh_token: String,
+    }
+
+    const ANILIST_BASE: &str = "https://anilist.co/api/v2/oauth/token";
+    let params = HashMap::from([
+        ("grant_type", "authorization_code"),
+        ("client_id", state.client_id.as_str()),
+        ("client_secret", state.client_secret.as_str()),
+        ("redirect_uri", "http://127.0.0.1:8000/authorized"),
+        ("code", code.as_str()),
+    ]);
+
+    let response = state
+        .client
+        .post(ANILIST_BASE)
+        .json(&params)
+        .send()
+        .await
+        .map_err(|e| BadRequest(Some(e.to_string())))?;
+
+    let body = response
+        .json::<TokenResponse>()
+        .await
+        .map_err(|e| BadRequest(Some(e.to_string())))?;
+
+    Ok(format!("{:#?}", body))
+}
+
 struct MyState {
     client_id: String,
+    client_secret: String,
+    client: reqwest::Client,
 }
 
 #[shuttle_service::main]
@@ -44,12 +98,22 @@ async fn rocket(
     let client_id = if let Some(client_id) = secret_store.get("ANILIST_CLIENT_ID") {
         client_id
     } else {
-        return Err(anyhow!("secret was not found").into());
+        return Err(anyhow!("Anilist Client ID was not found").into());
     };
 
-    let state = MyState { client_id };
+    let client_secret = if let Some(client_secret) = secret_store.get("ANILIST_SECRET") {
+        client_secret
+    } else {
+        return Err(anyhow!("Anilist Secret was not found").into());
+    };
+
+    let state = MyState {
+        client_id,
+        client_secret,
+        client: reqwest::Client::new(),
+    };
     let rocket = rocket::build()
-        .mount("/", routes![secret, login])
+        .mount("/", routes![secret, login, authorized])
         .manage(state);
 
     Ok(rocket)
